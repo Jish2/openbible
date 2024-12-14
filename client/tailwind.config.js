3482a96023ffcbcb4a48f9ed6243d2571725386d
Joshua Goon <j.j.jappiecraft@gmail.com>
2024-12-14 11:32:02 -0800

    feat: add dark mode colors to tw

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
+    colors: {
+      background: "var(--background-color)",
+      foreground: "var(--foreground-color)",
+    },
     boxShadow: {
       standard:
         "0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)",
