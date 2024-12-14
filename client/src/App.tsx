7d398dc92631115cf9248747db82e73ee999fecb
Joshua Goon <j.j.jappiecraft@gmail.com>
2024-12-13 22:54:05 -0800

    add example base

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,37 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import genesis from "../public/genesis.json";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="max-w-[30rem] p-4">
+        {genesis.chapters.map((chapter, index) => (
+          <div className="py-2">
+            <sup className="text-xs font-bold pr-1">{index + 1}</sup>
+
+            {chapter}
+          </div>
+        ))}
+        <h1 className="bg-red-200">Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque amet
+            obcaecati tenetur, similique quos voluptas nemo placeat a possimus,
+            earum veritatis cupiditate esse illo reiciendis? Delectus ipsam
+            fugit sint facere. Edit <code>src/App.tsx</code> and save to test
+            HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
