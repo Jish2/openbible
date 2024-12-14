53381679e72bf98b75b07a746cf285cb6426d295
Joshua Goon <j.j.jappiecraft@gmail.com>
2024-12-14 03:25:10 -0800

    add basic app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,38 +1,87 @@
-import { useState } from "react";
-import genesis from "../public/genesis.json";
+import { Fragment, useState } from "react";
+import john1 from "../../bible/books/john-1.json";
+import { cn } from "./utils/cn";
+import { MessageCircle } from "lucide-react";
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [showMenu, setShowMenu] = useState<boolean>(false);
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
       <div className="max-w-[30rem] p-4">
-        {genesis.chapters.map((chapter, index) => (
-          <div className="py-2">
-            <sup className="text-xs font-bold pr-1">{index + 1}</sup>
+        {john1.map(([key, verse], i) => {
+          const indent =
+            i !== 0 &&
+            (john1[i - 1][0] === "spacer" || john1[i - 1][0] === "title");
 
-            {chapter}
-          </div>
+          if (key === "spacer") return <br key={i} />;
+          if (key === "title")
+            return (
+              <h3 className="font-bold mt-5 mb-1" key={i}>
+                {verse}
+              </h3>
+            );
+
+          return <Verse key={i} number={key} verse={verse} indent={indent} />;
+        })}
+      </div>
+      <button
+        className="fixed bottom-0 left-0 p-4 bg-red-300"
+        onClick={() => setShowMenu((p) => !p)}
+      >
+        trigger
+      </button>
+      <div
+        className={cn(
+          showMenu
+            ? "animate-fly-in opacity-100"
+            : "animate-fly-out opacity-0 ",
+          "transition-opacity delay-0",
+          "fixed bottom-10 bg-white rounded-full shadow-aesthetic py-2 px-2 flex",
+        )}
+      >
+        {["🏠", "🖍️", "💬"].map((item) => (
+          <div className="px-2">{item}</div>
         ))}
-        <h1 className="bg-red-200">Vite + React</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque amet
-            obcaecati tenetur, similique quos voluptas nemo placeat a possimus,
-            earum veritatis cupiditate esse illo reiciendis? Delectus ipsam
-            fugit sint facere. Edit <code>src/App.tsx</code> and save to test
-            HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
       </div>
     </div>
   );
 }
 
 export default App;
+
+interface VerseProps {
+  number: string;
+  verse: string;
+  indent: boolean;
+}
+
+const Verse = ({ number, verse, indent }: VerseProps) => {
+  const [active, setActive] = useState<boolean>(false);
+
+  return (
+    <Fragment>
+      {indent && <div className="w-6 inline-block" />}
+      <span
+        className="py-2 cursor-pointer"
+        onClick={() => setActive((p) => !p)}
+      >
+        <sup className="text-xs text-gray-500 pr-1">{number}</sup>
+        <span
+          className={cn(
+            active && "underline decoration-dotted underline-offset-4",
+          )}
+        >
+          {verse}
+        </span>
+        <div className="w-1.5 inline-block" />
+      </span>
+      <div className="shadow-derek inline-block p-1 mr-2 rounded-md cursor-pointer relative">
+        <div className="bg-red-600 rounded-full size-2 absolute -top-0.5 -right-0.5" />
+        <div className="flex gap-1 items-center">
+          {/* <span className="text-xs">13</span> */}
+          <MessageCircle size={12} className="" />
+        </div>
+      </div>
+    </Fragment>
+  );
+};
